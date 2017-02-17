f1a90f1eab3ca277e9ac1a8c9474d3fd6e7b448e
Riley Watts <rileywatts.yes@gmail.com>
2017-02-17 10:54:23 -0800

    further develop scripts.js with if statements

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,41 @@ $(function() {
     var inputTwo = parseInt($("#questionTwo").val());
     var inputThree = parseInt($("#questionThree").val());
     var inputFour = parseInt($("#questionFour").val());
-
-    if (inputOne > 3) {
+    var inputFive = parseInt($("#questionFive").val());
+    console.log(inputOne);
+    if (inputOne !== 1 && inputOne !== 4) {
       $("#output").text(inputOne);
+      if ( 1 < inputFour && inputFour < 4 ) {
+        if (inputTwo+inputThree === 2) {
+          alert("microsoft");
+        }
+        else if (inputTwo+inputThree>2 && inputTwo+inputThree<5) {
+          alert("PHP");
+        }
+        else {
+          alert("ruby");
+        }
+
+      }
+      else if (inputFour === 4) {
+        $(".lover").show();
+        alert("lover");
+      }
+
+      else {
+        $(".groupComfort").show();
+        alert("here");
+      }
+
+
+    }
+    else if (inputOne === 4) {
+      $(".general").hide();
+      $(".seekHelp").show();
     }
     else {
-      alert("Hey");
+      $(".seekHelp").hide();
+      $(".general").show();
     }
   })
 });
